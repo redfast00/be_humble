97de921e1a5cad15c1dcb59fc4dc9618bdf1e4f4
redfast00 <redfast00@gmail.com>
2018-05-26 19:29:31 +0200

    Discord bot

---
+++ discord_bot.py
@@ -0,0 +1,35 @@
+import discord
+import asyncio
+from humblebundle import get_free_game_names
+from config import GAME_THREAD_CHANNEL_ID, TOKEN
+
+TIMEOUT = 60 * 60
+
+client = discord.Client()
+
+
+async def humble_background_task():
+    await client.wait_until_ready()
+    channel = discord.Object(id=GAME_THREAD_CHANNEL_ID)
+    already_seen = set()
+    while not client.is_closed:
+        print("checking new games")
+        free_games = set(get_free_game_names())
+        # Check if there are free games right now and if we haven't seen them
+        if free_games and not free_games.issubset(already_seen):
+            print("Sending message...")
+            message = '@here FREE right now: {} https://humblebundle.com/store/search?sort=discount'.format(' + '.join(free_games))
+            await client.send_message(channel, message)
+            already_seen.update(free_games)
+        await asyncio.sleep(TIMEOUT)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.loop.create_task(humble_background_task())
+client.run(TOKEN)
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from humblebundle import get_free_game_names
 if __name__ == '__main__':
     free_games = get_free_game_names()
     if free_games:
+        print('Free games: ' + ', '.join(free_games))
         with open('email_template.txt') as infile:
             content = infile.read()
         games = '\n'.join(free_games)
